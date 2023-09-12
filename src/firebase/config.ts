9af31204b01d4c19336ab7a8fbaa7d60375cd931
Đỗ Tuấn Đạt <datdt@dogoo.com.vn>
2023-09-12 15:45:21 +0700

    update inbox page

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,13 +7,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyAPXHRXzyZSSJwCmexX64rZfy3JM8J_HEM",
-	authDomain: "task-manager-chat-e6e92.firebaseapp.com",
-	projectId: "task-manager-chat-e6e92",
-	storageBucket: "task-manager-chat-e6e92.appspot.com",
-	messagingSenderId: "758420912822",
-	appId: "1:758420912822:web:c0bd1972156aac7ae3c417",
-	measurementId: "G-BGJ0EGN7P3",
+    apiKey: "AIzaSyAPXHRXzyZSSJwCmexX64rZfy3JM8J_HEM",
+    authDomain: "task-manager-chat-e6e92.firebaseapp.com",
+    projectId: "task-manager-chat-e6e92",
+    storageBucket: "task-manager-chat-e6e92.appspot.com",
+    messagingSenderId: "758420912822",
+    appId: "1:758420912822:web:c0bd1972156aac7ae3c417",
+    measurementId: "G-BGJ0EGN7P3",
 };
 
 // Initialize Firebase
