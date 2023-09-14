1e9a81d8313570c6179ba0e6e4ddb899c0bacb7d
Do Tuan Dat <dathymax@gmail.com>
2023-09-14 22:32:39 +0700

    update

--- src/pages/inbox/components/content/chat-board/index.tsx
+++ src/pages/inbox/components/content/chat-board/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import InboxContentChatItem from './ChatItem'
 import "./index.css"
+import { useInboxPageContext } from '../../../../../contexts/inbox';
+import { IInboxUser } from '../../sidebar/users/Users';
 
 const InboxContentChatBoard = () => {
+    let prevSender: any = null;
+    const { inboxById } = useInboxPageContext();
+    const messages: IInboxUser[] = inboxById.messages || [];
+
     return (
         <div className="p-3 relative" style={{ height: "calc(100% - 150px)", overflow: "hidden auto" }}>
-            <InboxContentChatItem />
+            {messages.map((message) => {
+                const isSameSender = prevSender && prevSender.id === message.name;
+
+                prevSender = message;
+
+                return (<InboxContentChatItem message={message} />)
+            })}
         </div>
     )
 }
